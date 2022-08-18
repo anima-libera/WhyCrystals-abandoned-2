fc1fd5eb55d86b6d539623382749c286eb3981d5
anima-libera <anim.libera@gmail.com>
2022-08-18 20:40:56 +0200

    Added window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
 
 #include <stdio.h>
+#include <SDL2/SDL.h>
 #include "embedded.h"
 
 int main(void)
 {
-	printf("hellow, %s\n", g_asset_test);
+	printf("hellow, %s", g_asset_test);
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		printf("TODO: Handle error.\n");
+		exit(-1);
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Pyea",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, 0);
+
+	SDL_Delay(1000);
+
+	SDL_DestroyWindow(window);
+	SDL_Quit(); 
+
 	return 0;
 }
