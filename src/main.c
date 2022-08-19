b63a618f20538a3bc74d88a2d2187fbb013398c1
anima-libera <anim.libera@gmail.com>
2022-08-19 03:19:26 +0200

    Added grid

--- src/main.c
+++ src/main.c
@@ -16,6 +16,21 @@ int main(void)
 
 	SDL_Window* window = SDL_CreateWindow("Pyea",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, 0);
+	if (window == NULL)
+	{
+		printf("TODO: Handle error.\n");
+		exit(-1);
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
+	if (renderer == NULL)
+	{
+		printf("TODO: Handle error.\n");
+		exit(-1);
+	}
+
+	int t = 0;
 
 	bool running = true;
 	while (running)
@@ -38,6 +53,23 @@ int main(void)
 				break;
 			}
 		}
+
+		t++;
+
+		SDL_SetRenderDrawColor(renderer, 30, 40, 80, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		for (int x = 0; x < 800; x += 40)
+		{
+			SDL_RenderDrawLine(renderer, x, 0, x, 800);
+		}
+		for (int y = 0; y < 800; y += 40)
+		{
+			SDL_RenderDrawLine(renderer, 0, y, 800, y);
+		}
+
+		SDL_RenderPresent(renderer);
 	}
 
 	SDL_DestroyWindow(window);
